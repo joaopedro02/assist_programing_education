a1e38d749527f2ea2f792ea2066be1e0ce24e41f
MarcosPauloRamos <49656714+MarcosPauloRamos@users.noreply.github.com>
2019-04-23 15:34:14 -0300

    Update add_questions_answers_data.py

--- educaton/questions/management/commands/add_questions_answers_data.py
+++ educaton/questions/management/commands/add_questions_answers_data.py
@@ -9,8 +9,10 @@ class Command(BaseCommand):
     def handle(self,*args,**options):
         q=Question(question_text="testando pergunta 1",form=1)
         q.save()
-        q.answers_set.create(answers_text="escolha 1",answers_value=1)
-        q.answers_set.create(answers_text="escolha 2",answers_value=1)
+        q.answers_set.create(answers_text="0",answers_value=1)
+        q.answers_set.create(answers_text="1",answers_value=1)
+        q.answers_set.create(answers_text="2",answers_value=1)
+        q.answers_set.create(answers_text="3",answers_value=1)
         
         q=Question(question_text="testando pergunta 2",form = 1 ) 
         q.save()
